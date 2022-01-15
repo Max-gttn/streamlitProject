9e62e3bc7ce573e02360da1ccf363a9df534dca5
Maxime Guitton <61966795+Max-gttn@users.noreply.github.com>
2022-01-15 17:10:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2636
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red31\green31\blue31;\red239\green239\blue239;}
-{\*\expandedcolortbl;;\cssrgb\c16078\c16078\c16078;\cssrgb\c94902\c94902\c94902;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh16380\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+mkdir -p ~/.streamlit/
 
-\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 mkdir -p ~/.streamlit/\cb1 \
-\cb3 echo \'93\\\cb1 \
-\cb3 [general]\\n\\\cb1 \
-\cb3 email = \\\'94\ul mguitt@gmail.com\ulnone \\\'94\\n\\\cb1 \
-\cb3 \'93 > ~/.streamlit/credentials.toml\cb1 \
-\cb3 echo \'93\\\cb1 \
-\cb3 [server]\\n\\\cb1 \
-\cb3 headless = true\\n\\\cb1 \
-\cb3 enableCORS=false\\n\\\cb1 \
-\cb3 port = $PORT\\n\\\cb1 \
-\cb3 \'93 > ~/.streamlit/config.toml\cb1 \
-}
+echo "\
+[general]\n\
+email = \"mguitt@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
